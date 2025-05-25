700a831f880a466393eb3b6c8df32ef1d2232c9f
Andreas M Lbn Gaol <lgandre45@gmail.com>
2025-05-25 15:02:20 +0700

    Update CORS allowed origin for local development
    Replaced "http://localhost:5178" with "http://localhost:3000" in the CORS configuration. This change aligns with the updated local development environment setup.

--- src/main/java/com/sanalab/sijiusu/core/config/CorsConfig.java
+++ src/main/java/com/sanalab/sijiusu/core/config/CorsConfig.java
@@ -16,7 +16,7 @@ public class CorsConfig {
             public void addCorsMappings(@NonNull CorsRegistry registry) {
                 registry.addMapping("/**")
                     .allowedOrigins(
-                        "http://localhost:5178",
+                        "http://localhost:3000",
                         "https://andreasmlbngaol.github.io"
                     )
                     .allowedMethods("GET", "POST", "PUT", "DELETE", "OPTIONS")
